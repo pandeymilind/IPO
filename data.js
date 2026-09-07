0bc5e2d15b97261b0a1003f573d5b3f485ba0259
Milind Pandey <milindshivampandey@gmail.com>
2026-09-07 12:45:17 +0530

    Auto-update IPO data

--- data.js
+++ data.js
@@ -4,57 +4,102 @@ const ipoData = {
             "Name": "Pranav Constructions",
             "Starting": "07 Sep 2026",
             "Ending": "09 Sep 2026",
-            "GMP_Percentage": "Not found"
+            "GMP_Percentage": "35.48%"
         },
         {
             "Name": "Kanohar Electricals",
             "Starting": "08 Sep 2026",
             "Ending": "10 Sep 2026",
-            "GMP_Percentage": "Not found"
+            "GMP_Percentage": "30.38%"
         },
         {
             "Name": "Prasol Chemicals",
             "Starting": "08 Sep 2026",
             "Ending": "10 Sep 2026",
-            "GMP_Percentage": "Not found"
+            "GMP_Percentage": "13.31%"
         },
         {
             "Name": "Glass Wall Systems",
             "Starting": "08 Sep 2026",
             "Ending": "10 Sep 2026",
-            "GMP_Percentage": "Not found"
+            "GMP_Percentage": "20.88%"
         },
         {
             "Name": "Asset Reconstruction",
             "Starting": "09 Sep 2026",
             "Ending": "11 Sep 2026",
-            "GMP_Percentage": "Not found"
+            "GMP_Percentage": "0.00%"
         },
         {
             "Name": "Karamtara Engineering",
             "Starting": "09 Sep 2026",
             "Ending": "11 Sep 2026",
-            "GMP_Percentage": "Not found"
+            "GMP_Percentage": "21.65%"
         },
         {
             "Name": "LCC Projects",
             "Starting": "09 Sep 2026",
             "Ending": "11 Sep 2026",
-            "GMP_Percentage": "Not found"
+            "GMP_Percentage": "17.12%"
         },
         {
             "Name": "Manipal Payment and Identity Solutions",
             "Starting": "09 Sep 2026",
             "Ending": "11 Sep 2026",
-            "GMP_Percentage": "Not found"
+            "GMP_Percentage": "10.03%"
         },
         {
             "Name": "Rentomojo",
             "Starting": "09 Sep 2026",
             "Ending": "11 Sep 2026",
-            "GMP_Percentage": "Not found"
+            "GMP_Percentage": "34.90%"
         }
     ],
-    "other_mainboard_ipos": [],
-    "last_updated": "2026-09-07 12:44:51"
+    "other_mainboard_ipos": [
+        {
+            "Name": "NSE",
+            "GMP_Percentage": "-%"
+        },
+        {
+            "Name": "Manika Plastech",
+            "GMP_Percentage": "16.28%"
+        },
+        {
+            "Name": "Veegaland Developers",
+            "GMP_Percentage": "21.43%"
+        },
+        {
+            "Name": "Steamhouse",
+            "GMP_Percentage": "0.00%"
+        },
+        {
+            "Name": "Rays of Belief",
+            "GMP_Percentage": "8.37%"
+        },
+        {
+            "Name": "Deepa Jewellers",
+            "GMP_Percentage": "13.28%"
+        },
+        {
+            "Name": "Purple Style Labs",
+            "GMP_Percentage": "-1.74%"
+        },
+        {
+            "Name": "ESDS Software Solution",
+            "GMP_Percentage": "72.49%"
+        },
+        {
+            "Name": "Priority Jewels",
+            "GMP_Percentage": "14.00%"
+        },
+        {
+            "Name": "Lumino Industries",
+            "GMP_Percentage": "46.34%"
+        },
+        {
+            "Name": "Annu Projects",
+            "GMP_Percentage": "-7.07%"
+        }
+    ],
+    "last_updated": "2026-09-07 12:45:17"
 };
