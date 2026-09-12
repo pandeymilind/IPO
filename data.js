fa2a651135cd710104319c5257a1032fe1e232b3
Milind Pandey <milindshivampandey@gmail.com>
2026-09-12 21:24:18 +0530

    Auto-update IPO data

--- data.js
+++ data.js
@@ -1,91 +1,83 @@
 const ipoData = {
     "groww_open_ipos": [
         {
-            "Name": "Asset Reconstruction",
-            "Starting": "09 Sep 2026",
-            "Ending": "11 Sep 2026",
-            "GMP_Percentage": "6.12%"
-        },
-        {
-            "Name": "Steamhouse India",
-            "Starting": "09 Sep 2026",
-            "Ending": "11 Sep 2026",
-            "GMP_Percentage": "28.09%"
-        },
-        {
-            "Name": "Karamtara Engineering",
-            "Starting": "09 Sep 2026",
-            "Ending": "11 Sep 2026",
-            "GMP_Percentage": "25.59%"
-        },
-        {
-            "Name": "LCC Projects",
-            "Starting": "09 Sep 2026",
-            "Ending": "11 Sep 2026",
-            "GMP_Percentage": "36.99%"
-        },
-        {
-            "Name": "Manipal Payment and Identity Solutions",
-            "Starting": "09 Sep 2026",
-            "Ending": "11 Sep 2026",
-            "GMP_Percentage": "0.29%"
-        },
-        {
-            "Name": "Rentomojo",
-            "Starting": "09 Sep 2026",
-            "Ending": "11 Sep 2026",
-            "GMP_Percentage": "34.16%"
-        },
-        {
             "Name": "Veegaland Developers",
             "Starting": "10 Sep 2026",
             "Ending": "15 Sep 2026",
-            "GMP_Percentage": "7.14%"
+            "GMP_Percentage": "10.71%"
         },
         {
             "Name": "Manika Plastech",
             "Starting": "11 Sep 2026",
             "Ending": "16 Sep 2026",
-            "GMP_Percentage": "23.26%"
+            "GMP_Percentage": "34.88%"
         }
     ],
     "other_mainboard_ipos": [
+        {
+            "Name": "A-One Steels",
+            "GMP_Percentage": "-%"
+        },
         {
             "Name": "Sonaselection India",
             "GMP_Percentage": "0.00%"
         },
         {
             "Name": "NSE",
-            "GMP_Percentage": "10.36%"
+            "GMP_Percentage": "11.65%"
         },
         {
             "Name": "SS Retail",
-            "GMP_Percentage": "7.08%"
-        },
-        {
-            "Name": "Jindal Supreme",
-            "GMP_Percentage": "22.58%"
+            "GMP_Percentage": "25.00%"
         },
         {
             "Name": "Hero Motors",
-            "GMP_Percentage": "9.52%"
+            "GMP_Percentage": "28.57%"
         },
         {
-            "Name": "Glass Wall Systems",
-            "GMP_Percentage": "26.37%"
+            "Name": "Jindal Supreme",
+            "GMP_Percentage": "26.88%"
         },
         {
-            "Name": "Prasol Chemicals",
-            "GMP_Percentage": "-2.22%"
+            "Name": "Manipal Payment and Identity Solutions",
+            "GMP_Percentage": "0.29%"
+        },
+        {
+            "Name": "Steamhouse",
+            "GMP_Percentage": "25.93%"
+        },
+        {
+            "Name": "LCC Projects",
+            "GMP_Percentage": "46.58%"
+        },
+        {
+            "Name": "Karamtara Engineering",
+            "GMP_Percentage": "27.17%"
+        },
+        {
+            "Name": "Rentomojo",
+            "GMP_Percentage": "39.60%"
+        },
+        {
+            "Name": "Asset Reconstruction",
+            "GMP_Percentage": "10.43%"
         },
         {
             "Name": "Kanohar Electricals",
             "GMP_Percentage": "37.97%"
         },
         {
+            "Name": "Glass Wall Systems",
+            "GMP_Percentage": "28.02%"
+        },
+        {
+            "Name": "Prasol Chemicals",
+            "GMP_Percentage": "-2.22%"
+        },
+        {
             "Name": "Pranav Constructions",
-            "GMP_Percentage": "35.48%"
+            "GMP_Percentage": "36.29%"
         }
     ],
-    "last_updated": "2026-09-11 14:08:39"
+    "last_updated": "2026-09-12 21:24:18"
 };
